dc19873d900fcc1702577d564486b8306f0509e8
Darryl Dixon <piecedigitalstudios@gmail.com>
2019-11-12 21:31:04 -0500

    initial commit

---
+++ CSGGPO/CSGGPO.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+#include "CSGGPO.h"
+
+using namespace CSGGPO;
+
+int GGPO:: Test()
+{
+	int txt = GGPOErrorCode::GGPO_OK;
+	return txt;
+}
---
+++ CSGGPO/CSGGPO.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include "ggponet.h"
+
+using namespace System;
+
+namespace CSGGPO {
+	public ref class GGPO
+	{
+	public:
+		int Test();
+	};
+}
